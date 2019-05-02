8a04f109acf315270358732d537754e5d30de975
CBaquero <cbm@di.uminho.pt>
2019-05-02 12:18:47 +0100

    added normal/gaussian distribution and key list pushing for some distributions

--- Random.cc
+++ Random.cc
@@ -13,6 +13,7 @@ extern "C" {
 std::random_device rd;
 std::mt19937 gen(rd());
 
+/* RANDOM.UNIF START END */
 int RandomUnif_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   if (argc != 3) return RedisModule_WrongArity(ctx);
   long long start, end;
@@ -28,6 +29,78 @@ int RandomUnif_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int a
   return REDISMODULE_OK;
 }
 
+/* RANDOM.NORM [MEAN=0.0] [STDDEV=1.0] */
+int RandomNorm_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  double mean=0.0, sd=1.0;
+  if (argc > 3) return RedisModule_WrongArity(ctx);
+  if (argc >= 2) /* Get mean */
+  {
+    if (RedisModule_StringToDouble(argv[1],&mean) != REDISMODULE_OK)
+      return RedisModule_ReplyWithError(ctx,"ERR invalid mean");
+  }
+  if (argc == 3) /* get stddev */
+  {
+    if (RedisModule_StringToDouble(argv[2],&sd) != REDISMODULE_OK)
+      return RedisModule_ReplyWithError(ctx,"ERR invalid standard deviation");
+  }
+
+  std::normal_distribution<double> rnorm(mean,sd);
+  RedisModule_ReplyWithDouble(ctx, rnorm(gen));
+  return REDISMODULE_OK;
+}
+
+/* RANDOM.LNORM KEY COUNT [MEAN=0.0] [STDDEV=1.0] */
+int RandomLNorm_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  double mean=0.0, sd=1.0;
+  if (argc > 5) return RedisModule_WrongArity(ctx);
+  if (argc >= 4) /* Get mean */
+  {
+    if (RedisModule_StringToDouble(argv[3],&mean) != REDISMODULE_OK)
+      return RedisModule_ReplyWithError(ctx,"ERR invalid mean");
+  }
+  if (argc == 5) /* get stddev */
+  {
+    if (RedisModule_StringToDouble(argv[4],&sd) != REDISMODULE_OK)
+      return RedisModule_ReplyWithError(ctx,"ERR invalid standard deviation");
+  }
+
+  /* Open key */
+  RedisModuleKey *key = (RedisModuleKey *) RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+ /* Key must be empty or list */
+  if ((RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_LIST &&
+       RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_EMPTY)) 
+  {
+     RedisModule_CloseKey(key);
+     return RedisModule_ReplyWithError(ctx,REDISMODULE_ERRORMSG_WRONGTYPE);
+  }
+
+  /* Get count */
+  long long count;
+  if ((RedisModule_StringToLongLong(argv[2],&count) != REDISMODULE_OK) ||
+        (count < 0)) 
+  {
+     RedisModule_CloseKey(key);
+     return RedisModule_ReplyWithError(ctx,"ERR invalid count");
+  }
+
+  /* Push count randoms */
+  std::normal_distribution<double> rnorm(mean,sd);
+  while (count-- > 0)
+  {
+    RedisModuleString *ele;
+    /* Make string with the same 19 digita precision of ReplyWithDouble */
+    ele=RedisModule_CreateStringPrintf(ctx,"%.19f",rnorm(gen));
+    RedisModule_ListPush(key,REDISMODULE_LIST_HEAD,ele);
+    RedisModule_FreeString(ctx,ele);
+  }
+ 
+  size_t len = RedisModule_ValueLength(key);
+  RedisModule_CloseKey(key);
+  RedisModule_ReplyWithLongLong(ctx, len);
+  return REDISMODULE_OK;
+}
+
+/* RANDOM.EXP [LAMBDA=1.0] */
 int RandomExp_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   double lambda;
   if (argc > 2) return RedisModule_WrongArity(ctx);
@@ -43,6 +116,55 @@ int RandomExp_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
   return REDISMODULE_OK;
 }
 
+/* RANDOM.LEXP KEY COUNT [LAMBDA=1.0] */
+int RandomLExp_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  double lambda;
+  if (argc < 3 || argc > 4) return RedisModule_WrongArity(ctx);
+  if (argc == 4)
+  {
+    if (RedisModule_StringToDouble(argv[3],&lambda) != REDISMODULE_OK)
+      return RedisModule_ReplyWithError(ctx,"ERR invalid lambda");
+  }
+  else // If no parameter, use default lambda of 1
+    lambda=1.0;
+
+  /* Open key */
+  RedisModuleKey *key = (RedisModuleKey *) RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+ /* Key must be empty or list */
+  if ((RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_LIST &&
+       RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_EMPTY)) 
+  {
+     RedisModule_CloseKey(key);
+     return RedisModule_ReplyWithError(ctx,REDISMODULE_ERRORMSG_WRONGTYPE);
+  }
+
+  /* Get count */
+  long long count;
+  if ((RedisModule_StringToLongLong(argv[2],&count) != REDISMODULE_OK) ||
+        (count < 0)) 
+  {
+     RedisModule_CloseKey(key);
+     return RedisModule_ReplyWithError(ctx,"ERR invalid count");
+  }
+
+  /* Push count randoms */
+  std::exponential_distribution<double> rexp(lambda);
+  while (count-- > 0)
+  {
+    RedisModuleString *ele;
+    /* Make string with the same 19 digita precision of ReplyWithDouble */
+    ele=RedisModule_CreateStringPrintf(ctx,"%.19f",rexp(gen));
+    RedisModule_ListPush(key,REDISMODULE_LIST_HEAD,ele);
+    RedisModule_FreeString(ctx,ele);
+  }
+ 
+  size_t len = RedisModule_ValueLength(key);
+  RedisModule_CloseKey(key);
+  RedisModule_ReplyWithLongLong(ctx, len);
+  return REDISMODULE_OK;
+}
+
+
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (RedisModule_Init(ctx,"random",1,REDISMODULE_APIVER_1)
@@ -52,9 +174,22 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
         RandomUnif_RedisCommand,"readonly random",0,0,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx,"random.norm",
+        RandomNorm_RedisCommand,"readonly random",0,0,0) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
+    if (RedisModule_CreateCommand(ctx,"random.lnorm",
+        RandomLNorm_RedisCommand,"random",0,0,0) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
     if (RedisModule_CreateCommand(ctx,"random.exp",
         RandomExp_RedisCommand,"readonly random",0,0,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
+
+    if (RedisModule_CreateCommand(ctx,"random.lexp",
+        RandomLExp_RedisCommand,"random",0,0,0) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
 
 
     return REDISMODULE_OK;
